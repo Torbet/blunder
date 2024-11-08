3fec3f4662f61703ccd1c0376e33a4b9cef14250
Guy Torbet <guy@torbet.co>
2024-11-08 17:28:54 +0000

    Update plotter

--- plot.py
+++ plot.py
@@ -10,15 +10,18 @@ limit = 10000
 results = os.listdir("results")
 results = [result for result in results if f"{limit}.npz" in result]
 
+# get plt colors as array
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
 # plot
 fig, ax = plt.subplots()
-for result in results:
+for i, result in enumerate(results):
     data = np.load(f"results/{result}")
     lossess = data["losses"]
     accuracies = data["accuracies"]
     model_name = result.split("-")[0]
-    ax.plot(lossess, label=f"{model_name} Loss")
-    ax.plot(accuracies, label=f"{model_name} Accuracy")
+    ax.plot(accuracies, label=f"{model_name} Accuracy", color=colors[i])
+    ax.plot(lossess, label=f"{model_name} Loss", color=colors[i], linestyle="dashed")
 ax.set_xlabel("Epoch")
 ax.set_ylabel("Loss / Accuracy")
 # legend smaller
